7d3ebd5a71e68f7cf00f6e5523dd57043db173dc
josht3113 <josht3113@yahoo.com>
2026-03-29 09:55:44 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // This must match your GitHub repo name exactly
-  base: '/context-climate/',
+  base: '/',
 })
