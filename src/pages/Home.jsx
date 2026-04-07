fc47437b9c73307cb1edc4efb9704639c6dcd1c2
josht3113 <josht3113@yahoo.com>
2026-04-07 11:50:45 -0400

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,7 +27,7 @@ const SECTIONS = [
         footerTags: ['Temp', 'Precipitation', 'Anomaly'],
         to:         '/annual',
         accentVar:  '--accent-live',
-        thumb: '/MonthlyThumbnail.png',
+        thumb: '/annualthumbnail.png',
       },
       {
         tags:       ['COMING SOON'],
