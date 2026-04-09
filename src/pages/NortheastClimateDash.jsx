1812628e1d6242238d62f750d36967fdf8650872
josht3113 <josht3113@yahoo.com>
2026-04-09 13:28:34 -0400

    Create NortheastClimateDash.jsx

---
+++ src/pages/NortheastClimateDash.jsx
@@ -0,0 +1,9 @@
+export default function NortheastClimate() {
+  return (
+    <iframe
+      src="https://josht3113.github.io/northeast-climate-dashboard/"
+      style={{ width:'100%', height:'100vh', border:'none', display:'block' }}
+      title="Northeast Climate Dashboard"
+    />
+  )
+}
