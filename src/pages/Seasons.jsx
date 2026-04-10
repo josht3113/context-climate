bd8a6949f5728940e3e1b3b1328857535f0e20aa
josht3113 <josht3113@yahoo.com>
2026-04-10 14:32:41 -0400

    Update Seasons.jsx

--- src/pages/Seasons.jsx
+++ src/pages/Seasons.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 export default function Seasons() {
   return (
     <iframe
-      src="https://josht3113.github.io/seasons-dashboard/"
+      src="/NortheastUS-seasons.html"
       style={{ width:'100%', height:'100vh', border:'none', display:'block' }}
       title="Seasons Analysis"
     />
