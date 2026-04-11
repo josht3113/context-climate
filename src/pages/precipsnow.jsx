d493bf086d1aa4f047cc3907b9580bb947b01b59
josht3113 <josht3113@yahoo.com>
2026-04-10 21:37:54 -0400

    Update precipsnow.jsx

--- src/pages/precipsnow.jsx
+++ src/pages/precipsnow.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 export default function YourPage() {
   return (
     <iframe
-      src="/your-file-name.html"
+      src="/precipsnow.html"
       style={{ width:'100%', height:'100vh', border:'none', display:'block' }}
       title="Your Tool Name"
     />
