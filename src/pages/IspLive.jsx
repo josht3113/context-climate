9c23ec004d6377c82f96a2c2a1fe45c97aab3358
josht3113 <josht3113@yahoo.com>
2026-04-11 13:29:56 -0400

    Update IspLive.jsx

--- src/pages/IspLive.jsx
+++ src/pages/IspLive.jsx
@@ -1,7 +1,7 @@
 export default function IspLive() {
   return (
     <iframe
-      src="https://YOUR-USERNAME.github.io/ISP-Live/"
+      src="https://josht3113.github.io/ISP-Live/"
       style={{ width: '100%', height: '100vh', border: 'none', display: 'block' }}
       title="ISP Live Weather"
     />
