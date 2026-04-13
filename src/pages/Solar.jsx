016ec22350404dc5bab8b78080b2abf3c6d30e89
josht3113 <josht3113@yahoo.com>
2026-04-12 21:53:20 -0400

    Add files via upload

--- src/pages/Solar.jsx
+++ src/pages/Solar.jsx
@@ -1,16 +1,9 @@
 export default function Solar() {
   return (
-    <div style={{ width: '100%', height: '100vh', overflow: 'hidden' }}>
-      <iframe
-        src="https://josht3113.github.io/Solar-Heatmap-Explorer/"
-        title="Solar Heatmap Explorer"
-        style={{
-          width: '100%',
-          height: '100%',
-          border: 'none',
-          display: 'block',
-        }}
-      />
-    </div>
-  );
+    <iframe
+      src="https://josht3113.github.io/Solar-Heatmap-Explorer/"
+      style={{ width: '100%', height: 'calc(100vh - 60px)', border: 'none', display: 'block' }}
+      title="Solar Heatmap Explorer"
+    />
+  )
 }
