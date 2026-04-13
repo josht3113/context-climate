66b2feb5d3d7a01501b07ee9b6d41eac866c2734
josht3113 <josht3113@yahoo.com>
2026-04-12 22:02:49 -0400

    Update Solar.jsx

--- src/pages/Solar.jsx
+++ src/pages/Solar.jsx
@@ -1,7 +1,7 @@
 export default function Solar() {
   return (
     <iframe
-      src="https://josht3113.github.io/Solar-Heatmap-Explorer/"
+      src="/solar-heatmap.html"
       style={{ width: '100%', height: 'calc(100vh - 60px)', border: 'none', display: 'block' }}
       title="Solar Heatmap Explorer"
     />
