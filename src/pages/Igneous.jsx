c7a7db0e33dabfb77e62372e7b4bd4c2db167333
josht3113 <josht3113@yahoo.com>
2026-04-16 22:50:53 -0400

    Update Igneous.jsx

--- src/pages/Igneous.jsx
+++ src/pages/Igneous.jsx
@@ -1,9 +1,3 @@
-export default function Solar() {
-  return (
-    <iframe
-      src="/igneous_crystallization.html"
-      style={{ width: '100%', height: 'calc(100vh - 60px)', border: 'none', display: 'block' }}
-      title="Solar Heatmap Explorer"
-    />
-  )
+export default function Igneous() {
+  return <iframe src="/igneous_crystallization.html" style={{ width:'100%', height:'100vh', border:'none' }} title="Igneous Crystallization Simulator" />
 }
