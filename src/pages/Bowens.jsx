3823f33f447182aca88145c4e80df25907281a28
josht3113 <josht3113@yahoo.com>
2026-04-18 20:03:52 -0400

    Update Bowens.jsx

--- src/pages/Bowens.jsx
+++ src/pages/Bowens.jsx
@@ -1,6 +1,6 @@
 export default function Bowens() {
   return (
-    <div style={{ display: 'flex', flexDirection: 'column', height: 'calc(100vh - var(--header-height))', width: '100%' }}
+    <div style={{ display: 'flex', flexDirection: 'column', height: 'calc(100vh - var(--header-height))', width: '100%' }}>
       <iframe
         src="/bowens_simulator.html"
         title="Bowen's Reaction Series Simulator"
