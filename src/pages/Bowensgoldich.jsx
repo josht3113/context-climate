fc173bea7dcc3279f4b4499391588e8ec2772d04
josht3113 <josht3113@yahoo.com>
2026-04-20 19:16:14 -0400

    Delete src/pages/Bowensgoldich.jsx

--- src/pages/Bowensgoldich.jsx
+++
@@ -1,9 +0,0 @@
-export default function BowensGoldich() {
-  return (
-    <iframe
-      src="/bowens_goldich.html"
-      title="Bowen's Reaction Series & Goldich Stability Series"
-      style={{ width: '100%', height: 'calc(100vh - 60px)', border: 'none', display: 'block' }}
-    />
-  )
-}
