44251bdcb546e170ca9c77f3be26acffdfe559e0
josht3113 <josht3113@yahoo.com>
2026-04-26 09:24:24 -0400

    Create ESSRT_8_9_10.jsx

---
+++ src/pages/ESSRT_8_9_10.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+export default function ESSRT_8_9_10() {
+  return (
+    <iframe
+      src="/ESSRT_8_9_10.html"
+      style={{ width:'100%', height:'100vh', border:'none', display:'block' }}
+      title="NY State ESSRT Maps and Topography"
+    />
+  )
+}
