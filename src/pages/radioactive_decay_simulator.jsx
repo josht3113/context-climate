d3a6bbebd473a5080573620d355cf1a4409c30fe
josht3113 <josht3113@yahoo.com>
2026-05-02 13:19:45 -0400

    Update radioactive_decay_simulator.jsx

--- src/pages/radioactive_decay_simulator.jsx
+++ src/pages/radioactive_decay_simulator.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 
-export default function (radioactive_decay_simulator) {
+export default function radioactive_decay_simulator() {
   return (
     <iframe
-      src="/radioactive_decay_simulator.html"
+      src={`${process.env.PUBLIC_URL}/radioactive_decay_simulator.html`}
       style={{ width:'100%', height:'100vh', border:'none', display:'block' }}
       title="Radioactive Decay Simulator"
     />
