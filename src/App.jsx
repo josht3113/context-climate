88b3744caf378a01ae3a4ce0d76d4222344f3f16
josht3113 <josht3113@yahoo.com>
2026-05-02 12:53:27 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
 import { HashRouter, Routes, Route } from 'react-router-dom'
-import Header from './components/Header'
-import Home             from './pages/Home'
-import HourlyData       from './pages/HourlyData'
-import Climate          from './pages/Climate'
-import ENSO             from './pages/ENSO'
-import Seasons          from './pages/Seasons'
-import IspLive          from './pages/IspLive'
-import PrecipSnow       from './pages/PrecipSnow'
-import NortheastClimate from './pages/NortheastClimate'
-import Hurricanes       from './pages/Hurricanes'
-import Solar            from './pages/Solar'
-import Igneous          from './pages/Igneous'
-import ScrollToTop      from './components/ScrollToTop'
-import EarthAndSpace    from './pages/EarthAndSpace'
-import Bowens           from './pages/Bowens'
-import Metamorphic      from './pages/Metamorphic'
-import StreamTransport  from './pages/StreamTransport'
-import BowensGoldich    from './pages/BowensGoldich'
-import ESSRT_8_9_10     from './pages/ESSRT_8_9_10'
-import ESSRT_6_7        from './pages/ESSRT_6_7'
+import Header                      from './components/Header'
+import Home                        from './pages/Home'
+import HourlyData                  from './pages/HourlyData'
+import Climate                     from './pages/Climate'
+import ENSO                        from './pages/ENSO'
+import Seasons                     from './pages/Seasons'
+import IspLive                     from './pages/IspLive'
+import PrecipSnow                  from './pages/PrecipSnow'
+import NortheastClimate            from './pages/NortheastClimate'
+import Hurricanes                  from './pages/Hurricanes'
+import Solar                       from './pages/Solar'
+import Igneous                     from './pages/Igneous'
+import ScrollToTop                 from './components/ScrollToTop'
+import EarthAndSpace               from './pages/EarthAndSpace'
+import Bowens                      from './pages/Bowens'
+import Metamorphic                 from './pages/Metamorphic'
+import StreamTransport             from './pages/StreamTransport'
+import BowensGoldich               from './pages/BowensGoldich'
+import ESSRT_8_9_10                from './pages/ESSRT_8_9_10'
+import ESSRT_6_7                   from './pages/ESSRT_6_7'
+import Radioactive Decay           from './pages/Radioactive_decay_simulator'
 
 // ── App shell ─────────────────────────────────────────
 // HashRouter is used so that GitHub Pages works correctly
@@ -36,24 +37,25 @@ export default function App() {
   <Header />
       <main style={{ overflowX: 'hidden' }}>
         <Routes>
-          <Route path="/"                  element={<Home />}             />
-          <Route path="/isp-live"          element={<IspLive />}          />
-          <Route path="/precipsnow"        element={<PrecipSnow />}       />
-          <Route path="/northeast-climate" element={<NortheastClimate />} />
-          <Route path="/seasons"           element={<Seasons />}          />
-          <Route path="/monthly"           element={<HourlyData />}       />
-          <Route path="/annual"            element={<Climate />}          />
-          <Route path="/enso"              element={<ENSO />}             />
-          <Route path="/hurricanes"        element={<Hurricanes />}       />
-          <Route path="/solar"             element={<Solar />}            />
-          <Route path="/igneous"           element={<Igneous />}          />
-          <Route path="/earthandspace"     element={<EarthAndSpace />}    />
-          <Route path="/bowens"            element={<Bowens />}           />
-          <Route path="/metamorphic"       element={<Metamorphic />}      />
-          <Route path="/stream-transport"  element={<StreamTransport />}  />
-          <Route path="/bowensgoldich"     element={<BowensGoldich />}    />
-          <Route path="/ESSRT_8_9_10"      element={<ESSRT_8_9_10 />}     />
-          <Route path="/ESSRT_6_7"         element={<ESSRT_6_7 />}        />
+          <Route path="/"                                    element={<Home />}             />
+          <Route path="/isp-live"                            element={<IspLive />}          />
+          <Route path="/precipsnow"                          element={<PrecipSnow />}       />
+          <Route path="/northeast-climate"                   element={<NortheastClimate />} />
+          <Route path="/seasons"                             element={<Seasons />}          />
+          <Route path="/monthly"                             element={<HourlyData />}       />
+          <Route path="/annual"                              element={<Climate />}          />
+          <Route path="/enso"                                element={<ENSO />}             />
+          <Route path="/hurricanes"                          element={<Hurricanes />}       />
+          <Route path="/solar"                               element={<Solar />}            />
+          <Route path="/igneous"                             element={<Igneous />}          />
+          <Route path="/earthandspace"                       element={<EarthAndSpace />}    />
+          <Route path="/bowens"                              element={<Bowens />}           />
+          <Route path="/metamorphic"                         element={<Metamorphic />}      />
+          <Route path="/stream-transport"                    element={<StreamTransport />}  />
+          <Route path="/bowensgoldich"                       element={<BowensGoldich />}    />
+          <Route path="/ESSRT_8_9_10"                        element={<ESSRT_8_9_10 />}     />
+          <Route path="/ESSRT_6_7"                           element={<ESSRT_6_7 />}        />
+          <Route path="/radioactive_decay_simulator"         element={<radioactive_decay_simulatpr />}        />
         </Routes>
       </main>
       <footer style={{
