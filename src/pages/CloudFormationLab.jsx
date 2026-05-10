dce18b5a6416f35b1e9e5537ec46e316b546c179
josht3113 <josht3113@yahoo.com>
2026-05-10 17:52:06 -0400

    Add files via upload

---
+++ src/pages/CloudFormationLab.jsx
@@ -0,0 +1,9 @@
+export default function CloudFormationLab() {
+  return (
+    <iframe
+      src="/cloud_formation_lab.html"
+      style={{ width: '100%', height: 'calc(100vh - 60px)', border: 'none', display: 'block' }}
+      title="Cloud Formation Lab"
+    />
+  );
+}
---
+++ src/pages/SeismicWaveExplorer.jsx
@@ -0,0 +1,9 @@
+export default function SeismicWaveExplorer() {
+  return (
+    <iframe
+      src="/seismic_wave_explorer.html"
+      style={{ width: '100%', height: 'calc(100vh - 60px)', border: 'none', display: 'block' }}
+      title="Seismic Wave Explorer"
+    />
+  );
+}
