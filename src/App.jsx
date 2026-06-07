4966ec38438357f8d9e6b1ebe139d32d463ef405
josht3113 <josht3113@yahoo.com>
2026-06-07 18:20:21 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -49,6 +49,7 @@ import TempHeatmapAnnual           from './pages/TempHeatmapAnnual'
 import PrecipHeatmapMonthly        from './pages/PrecipHeatmapMonthly'
 import PrecipHeatmapAnnual         from './pages/PrecipHeatmapAnnual'
 import EnsoHeatmap                 from './pages/EnsoHeatmap'
+import EnsoSpaghetti               from './pages/EnsoSpaghetti'
 
 // ── App shell ─────────────────────────────────────────
 // HashRouter is used so that GitHub Pages works correctly
@@ -114,6 +115,7 @@ export default function App() {
           <Route path="/precip-heatmap-monthly"              element={<PrecipHeatmapMonthly />}    />
           <Route path="/precip-heatmap-annual"               element={<PrecipHeatmapAnnual />}     />
           <Route path="/enso-heatmap"                        element={<EnsoHeatmap />}             />
+          <Route path="/enso-spaghetti"                      element={<EnsoSpaghetti />}           />
         </Routes>
       </main>
       <footer style={{
