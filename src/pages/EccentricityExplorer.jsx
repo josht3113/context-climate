099d74aacba753a8eb0dbccf32d6c305890d8f14
josht3113 <josht3113@yahoo.com>
2026-06-13 12:42:23 -0400

    Add files via upload

---
+++ src/pages/EccentricityExplorer.jsx
@@ -0,0 +1,9 @@
+export default function EccentricityExplorer() {
+  return (
+    <iframe
+      src="/eccentricity-explorer.html"
+      style={{ width: '100%', height: 'calc(100vh - 56px)', border: 'none', display: 'block' }}
+      title="Eccentricity Explorer"
+    />
+  )
+}
---
+++ src/pages/StackedSignal.jsx
@@ -0,0 +1,9 @@
+export default function StackedSignal() {
+  return (
+    <iframe
+      src="/stacked-signal.html"
+      style={{ width: '100%', height: 'calc(100vh - 56px)', border: 'none', display: 'block' }}
+      title="The Stacked Signal"
+    />
+  )
+}
