bc64f9eea7be419128d8a0c3ffa2f8e11e6839d7
josht3113 <josht3113@yahoo.com>
2026-06-12 20:00:23 -0400

    Add files via upload

---
+++ src/pages/ObliquityExplorer.jsx
@@ -0,0 +1,9 @@
+export default function ObliquityExplorer() {
+  return (
+    <iframe
+      src="/obliquity-explorer.html"
+      style={{ width: '100%', height: 'calc(100vh - 56px)', border: 'none', display: 'block' }}
+      title="Obliquity Explorer"
+    />
+  )
+}
