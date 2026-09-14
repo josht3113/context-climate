0c9b7b63334bfa35dd136a8af445359f4559b6f8
josht3113 <josht3113@yahoo.com>
2026-09-14 12:51:21 -0400

    Delete public/TropicalACE.jsx

--- public/TropicalACE.jsx
+++
@@ -1,9 +0,0 @@
-export default function TropicalACE() {
-  return (
-    <iframe
-      src={`${import.meta.env.BASE_URL}tropical-ace.html`}
-      style={{ width: '100%', height: 'calc(100vh - 60px)', border: 'none', display: 'block' }}
-      title="Tropical ACE Tracker"
-    />
-  )
-}
